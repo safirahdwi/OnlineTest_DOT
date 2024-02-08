46bf797c98dd4407e1669c559c14353ed0f634c8
DWI <safirah988@gmail.clm>
2024-02-08 13:50:52 +0700

    Login

---
+++ Object Repository/Login_Reqres.rs
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Login_Reqres</name>
+   <tag></tag>
+   <elementGuidId>b83865f4-3a9c-43f4-b4c7-6afa2585b6f9</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>false</autoUpdateContent>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n    \&quot;email\&quot;: \&quot;eve.holt@reqres.in\&quot;,\n    \&quot;password\&quot;: \&quot;cityslicka\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>92b9ef24-496d-4023-ac0c-68c90c207847</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Basic dGVsa29tOmRhMWMyNWQ4LTM3YzgtNDFiMS1hZmUyLTQyZGQ0ODI1YmZlYQ==</value>
+      <webElementGuid>a35e8276-3e42-49ac-bb1f-66cb3b826e25</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>7.5.11</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://reqres.in/api/login</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Scripts/Reqres/Login Success/Script1707371185262.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.TestObjectProperty
+import com.kms.katalon.core.testobject.impl.HttpTextBodyContent
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
+RequestObject verifyBasic = findTestObject('Object Repository/Login_Reqres')
+verifyBasic.setRestUrl("https://reqres.in/api/login")
+
+ArrayList<TestObjectProperty> HTTPHeader = new ArrayList<TestObjectProperty>()
+HTTPHeader.add(new TestObjectProperty('Content-Type', ConditionType.EQUALS, 'application/json'))
+verifyBasic.setHttpHeaderProperties(HTTPHeader)
+
+WS.comment('Login Success')
+
+String requestBody = """{
+    "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
+}"""
+verifyBasic.setBodyContent(new HttpTextBodyContent(requestBody))
+
+'send request'
+def responseVerifyBasic = WS.sendRequest(verifyBasic)
+
+'Verify Respon'
+def hasil = WS.verifyResponseStatusCode(responseVerifyBasic, 200)
+
+def slurper = new groovy.json.JsonSlurper()
+def result = slurper.parseText(responseVerifyBasic.getResponseBodyContent())
+println result
+
+
+
+
